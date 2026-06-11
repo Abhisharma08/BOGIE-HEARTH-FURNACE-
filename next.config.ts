f6da5629d97e403b7be97f35d76274ced93e4701
Abhisharma08 <aksharma223@gmail.com>
2026-06-11 15:24:30 +0530

    chore: update next.config.ts trailing slash configuration and remove unused middleware file

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   basePath: '/bogie-hearth-furnace',
-  skipTrailingSlashRedirect: true,
+  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
