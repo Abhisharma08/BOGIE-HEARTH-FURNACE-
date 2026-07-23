bfe3a0c05627facc40e6e1d5d8059803e5e6e9bf
charu <charup640@gmail.com>
2026-07-23 10:28:20 +0530

    Resolve merge conflicts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
   basePath: '/heat-treatment-furnaces',
-=======
-  basePath: '/bogie-hearth-furnace',
->>>>>>> e4d0f9664cab319f6892cea503f7db3a91570d02
   skipTrailingSlashRedirect: true,
   typescript: {
     ignoreBuildErrors: true,
